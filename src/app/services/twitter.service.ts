637363f6a4b37e62e83856dde1a5420caf1f892b
Samuel Perez <samusinho79@gmail.com>
2019-08-01 19:52:16 -0500

    se actualiza la direccion del backend para realizar pruebas y consultas

--- src/app/services/twitter.service.ts
+++ src/app/services/twitter.service.ts
@@ -7,14 +7,14 @@ import { AlertService } from './alert.service';
   providedIn: 'root'
 })
 export class TwitterService {
-  serverUrl: string = 'http://localhost:3000'; //guardar valor en variable de entorno luego
+  serverUrl: string = 'https://samuel-tweet-searcher.herokuapp.com/'; //guardar valor en variable de entorno luego
 
   constructor(private http: HttpClient, private _alert: AlertService) {
 
   }
 
   getTweets() {
-    return this.http.get<any>(`${this.serverUrl}/tweets`).pipe(
+    return this.http.get<any>(`${this.serverUrl}tweets`).pipe(
       map(data => data),
       catchError(err => {
         this._alert.showError(err);
@@ -24,7 +24,7 @@ export class TwitterService {
   }
 
   searchTweets(word: string) {
-    return this.http.get<any>(`${this.serverUrl}/search/${word}`).pipe(
+    return this.http.get<any>(`${this.serverUrl}search/${word}`).pipe(
       map(data => data),
       catchError(err => {
         this._alert.showError(err);
@@ -34,7 +34,7 @@ export class TwitterService {
   }
 
   favorite(id: string) {
-    return this.http.post<any>(`${this.serverUrl}/favorites/create/${id}`, {
+    return this.http.post<any>(`${this.serverUrl}favorites/create/${id}`, {
       id: id
     }).pipe(
       map(data => data),
@@ -46,7 +46,7 @@ export class TwitterService {
   }
 
   unfavorite(id: string) {
-    return this.http.post<any>(`${this.serverUrl}/favorites/destroy/${id}`, {
+    return this.http.post<any>(`${this.serverUrl}favorites/destroy/${id}`, {
       id: id
     }).pipe(
       map(data => data),
@@ -58,7 +58,7 @@ export class TwitterService {
   }
 
   retweet(id: string) {
-    return this.http.post<any>(`${this.serverUrl}/retweet/${id}`, {
+    return this.http.post<any>(`${this.serverUrl}retweet/${id}`, {
       id: id
     }).pipe(
       map(data => data),
@@ -70,7 +70,7 @@ export class TwitterService {
   }
 
   unretweet(id: string) {
-    return this.http.post<any>(`${this.serverUrl}/unretweet/${id}`, {
+    return this.http.post<any>(`${this.serverUrl}unretweet/${id}`, {
       id: id
     }).pipe(
       map(data => data),
